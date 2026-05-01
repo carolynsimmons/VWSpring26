c9e581a28bd22c86e221bb6f0feac25d417a627c
carolynsimmons <crs255@miami.edu>
2026-05-01 18:02:52 -0400

    Updated interaction

--- My project/Assets/Scripts/SceneChanger.cs
+++ My project/Assets/Scripts/SceneChanger.cs
@@ -4,19 +4,8 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    SceneManager sceneManager;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void changeScene(string sceneName)
+   SceneManager sceneManager;
+   public void changeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
